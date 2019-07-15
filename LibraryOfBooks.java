0db234b36b05d0a0786738a8c9966c5b7f756011
Robbie Gill <34260042+robbieGill4096@users.noreply.github.com>
2019-07-15 16:27:50 -0600

    Add files via upload

--- LibraryOfBooks.java
+++ LibraryOfBooks.java
@@ -27,6 +27,8 @@ public class LibraryOfBooks {
 	// general purpose
 	static String invalidPrompt = "Invalid Selection!";
 	static String dividerPrompt = "==================================";
+	static LibraryInterface littleLibrary = new Library();
+	static Scanner reader = new Scanner(System.in);
 
 	public static void menuPrompt() {
 		System.out.println(borderPrompt);
@@ -41,13 +43,158 @@ public class LibraryOfBooks {
 		System.out.print(selectionPrompt);
 	}
 
+	public static void content() {
+		System.out.println("View contents of the Library....");
+		System.out.println(dividerPrompt);
+
+		String bookList = littleLibrary.toString();
+
+		System.out.println(bookList);
+		System.out.print(selectionPrompt);
+
+	}
+
+	public static void addBook() {
+		System.out.println("Add a book to the library....\n");
+		System.out.print("Please enter the books Title: ");
+		String title = reader.nextLine();
+		System.out.print("Please enter the books Author: ");
+		String author = reader.nextLine();
+		Book newBook = new Book(title, author);
+
+		System.out.print("Please enter the books Genre: ");
+		String genre = reader.nextLine();
+		newBook.setGenre(genre);
+		System.out.print("Please enter the books filename: ");
+		String filename = reader.nextLine();
+		newBook.setFilename(filename);
+
+		littleLibrary.addBook(newBook);
+
+		System.out.print(selectionPrompt);
+
+	}
+
+	public static void deleteBook() {
+		System.out.println("Remove a book from the library...\n");
+		System.out.print("Please enter the index of the book: ");
+		try {
+			String index = reader.nextLine();
+
+			Integer number = Integer.parseInt(index);
+			int sizeOfLib = littleLibrary.getBooks().size();
+			if (number < sizeOfLib) {
+
+				littleLibrary.removeBook(number);
+
+				System.out.println();
+				System.out.println("Book at index " + number + " removed from Library.");
+				System.out.print(selectionPrompt);
+				return;
+			} else {
+				System.out.println(number + " is not an index in the current Library. ");
+				System.out.print(selectionPrompt);
+				return;
+			}
+		} catch (Exception e) {
+			System.out.println(invalidPrompt);
+			System.out.print(selectionPrompt);
+			return;
+
+		}
+	}
+
+	public static String getPage(String fulltext, int index) {
+		ArrayList<String> bookPages = new ArrayList<String>();
+		final int INCREMENT = 1500;
+		int firstIndex = 0;
+		int lastIndex = INCREMENT;
+		int count = 0;
+		while (lastIndex > count) {
+			count += 1;
+			fulltext += " ";
+		}
+		do {
+
+			bookPages.add(fulltext.substring(firstIndex, lastIndex));
+
+			firstIndex += INCREMENT;
+			lastIndex += INCREMENT;
+		} while (lastIndex < fulltext.length());
+
+		String selectedPage = bookPages.get(index);
+		return selectedPage;
+
+	}
+
+	public static void readBook() {
+		try {
+			System.out.println("Read a book from the library...\n");
+			System.out.print("Please enter the index of the book: ");
+
+			String index = reader.nextLine();
+			Integer number = Integer.parseInt(index);
+			int sizeOfLib = littleLibrary.getBooks().size();
+			if (number < sizeOfLib) {
+
+				Book selectedBook = littleLibrary.getBook(number);
+				System.out.println("How would you like to read the text:\n(S)croll\n(P)age");
+				String userSelection = reader.nextLine();
+
+				if (userSelection.toLowerCase().equals("s")) {
+					System.out.println(selectedBook.getText());
+					System.out.print(selectionPrompt);
+					return;
+				}
+				if (userSelection.toLowerCase().equals("p")) {
+					System.out.print("Please enter a page number:");
+					String pageNum = reader.nextLine();
+					Integer pageNumber = Integer.parseInt(pageNum);
+					System.out.println(getPage(selectedBook.getText(), pageNumber));
+					System.out.println();
+					System.out.println("Would you like to keep reading? (Y) or (N)");
+					String response = "";
+
+					while (response.toLowerCase().equals("n") != true) {
+						response = "";
+						String input = reader.nextLine();
+						if (input.equals("n") == true) {
+							break;
+						}
+						pageNumber += 1;
+
+						System.out.println(getPage(selectedBook.getText(), pageNumber));
+						System.out.println();
+						System.out.println("Would you like to keep reading? (Y) or (N)");
+						response += input;
+
+					}
+					System.out.print(selectionPrompt);
+					return;
+				}
+
+			} else {
+				System.out.println(number + " is not an index in the current Library. ");
+				System.out.print(selectionPrompt);
+				return;
+			}
+
+		}
+
+		catch (Exception e) {
+			System.out.println(invalidPrompt);
+			System.out.print(selectionPrompt);
+			return;
+
+		}
+	}
+
+
 	public static void mainMenu() {
-		LibraryInterface littleLibrary = new Library();
 		String selection = "";
 		while (selection.equals("Q") != true) {
 			selection = "";
 
-			Scanner reader = new Scanner(System.in);
 			String input = reader.nextLine();
 
 			selection += input.toUpperCase();
@@ -55,89 +202,19 @@ public class LibraryOfBooks {
 			switch (selection.toUpperCase()) {
 			// view contents of the library.
 			case "C":
-				System.out.println("View contents of the Library....");
-				System.out.println(dividerPrompt);
-
-				String bookList = littleLibrary.toString();
-
-				System.out.println(bookList);
-				System.out.print(selectionPrompt);
+				content();
 				break;
-			// add a book to the library.
 			case "A":
-				System.out.println("Add a book to the library....\n");
-				System.out.print("Please enter the books Title: ");
-				String title = reader.nextLine();
-				System.out.print("Please enter the books Author: ");
-				String author = reader.nextLine();
-				Book newBook = new Book(title, author);
-
-				System.out.print("Please enter the books Genre: ");
-				String genre = reader.nextLine();
-				newBook.setGenre(genre);
-				System.out.print("Please enter the books filename: ");
-				String filename = reader.nextLine();
-				newBook.setFilename(filename);
-
-				littleLibrary.addBook(newBook);
-
-				System.out.print(selectionPrompt);
+				addBook();
 				break;
-			// delete a book from the library.
 			case "D":
-				System.out.println("Remove a book from the library...\n");
-				System.out.print("Please enter the index of the book: ");
-				try {
-					String index = reader.nextLine();
-
-					Integer number = Integer.parseInt(index);
-					int sizeOfLib = littleLibrary.getBooks().size();
-					if (number < sizeOfLib) {
-
-						littleLibrary.removeBook(number);
-
-						System.out.println();
-						System.out.println("Book at index " + number + " removed from Library.");
-						System.out.print(selectionPrompt);
-						break;
-					} else {
-						System.out.println(number + " is not an index in the current Library. ");
-						System.out.print(selectionPrompt);
-						break;
-					}
-				} catch (Exception e) {
-					System.out.println(invalidPrompt);
-					System.out.print(selectionPrompt);
-					break;
-
-				}
-			//read a book in the library.
+				deleteBook();
+				break;
+			// read a book in the library.
 			case "R":
-				System.out.println("Read a book from the library...\n");
-				System.out.print("Please enter the index of the book: ");
-				try {
-					String index = reader.nextLine();
-
-					Integer number = Integer.parseInt(index);
-					int sizeOfLib = littleLibrary.getBooks().size();
-					if (number < sizeOfLib) {
-
-						Book selectedBook = littleLibrary.getBook(number);
-						System.out.println(selectedBook.getText());
-						System.out.print(selectionPrompt);
-						break;
-					} else {
-						System.out.println(number + " is not an index in the current Library. ");
-						System.out.print(selectionPrompt);
-						break;
-					}
-				} catch (Exception e) {
-					System.out.println(invalidPrompt);
-					System.out.print(selectionPrompt);
-					break;
-
-				}
-			//display Menu again.
+				readBook();
+				break;
+			// display Menu again.
 			case "M":
 				menuPrompt();
 				break;
@@ -150,6 +227,7 @@ public class LibraryOfBooks {
 			}
 		}
 		System.out.println("Goodbye!");
+		reader.close();
 	}
 
 	public static void main(String[] args) {
